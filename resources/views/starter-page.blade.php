a4f93189e570d8199ac0e464de56ce7ab78d7bd7
Nilfgard13 <fachrizalfazza@gmail.com>
2024-09-13 09:09:42 +0700

    second commit

---
+++ resources/views/starter-page.blade.php
@@ -0,0 +1,11 @@
+<x-layout_landing>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <!-- Starter Section Section -->
+    <section id="starter-section" class="starter-section section">
+
+        <div class="container" data-aos="fade-up">
+            <p>Use this page as a starter for your own custom pages.</p>
+        </div>
+
+    </section><!-- /Starter Section Section -->
+</x-layout_landing>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,29 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index', );
+});
+
+Route::get( '/about', function () {
+    return view('about', ['title' => 'About']);
+});
+
+Route::get('/services', function () {
+    return view('services', ['title' => 'Services']);
+});
+
+Route::get('/projects', function () {
+    return view('projects', ['title' => 'Projects']);
+});
+
+Route::get('/blog', function () {
+    return view('blog', ['title' => 'Blog']);
+});
+
+// Route::get('/Dropdown', function () {
+//     return view('index');
+// });
+
+Route::get('/contact', function () {
+    return view('contact', ['title' => 'Contact']);
 });
